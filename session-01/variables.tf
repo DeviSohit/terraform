f8b70897a6b2337a29d86749e6b48984a22be890
devi <devimeena.iiit@gmail.com>
2023-09-08 09:23:16 +0530

    terraform

---
+++ session-01/variables.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    type = string
+    default = "ami-03265a0778a880afb" 
+}
+
+variable "instance_type" {
+    #type = string #if you don't give this, terraform will understand automatically data types
+    default = "t2.micro" 
+}
+
+variable "sg_name" {
+    default = "allow-all"
+}
+
+variable "sg_cidr" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
